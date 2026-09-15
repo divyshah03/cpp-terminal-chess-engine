0cffa681f29e15a360b9f51dc3816fc31f7ecdf5
Divy Shah <divyrshah3@gmail.com>
2026-09-15 19:43:38 -0400

    enumerated: implement opponentOf, pieceValue and the character helpers

--- enumerated.cc
+++ enumerated.cc
@@ -1,7 +1,45 @@
 #include "enumerated.h"
+#include <cctype>
 #include <iostream>
 
 using namespace std;
+
+Colour opponentOf(Colour colour){
+    if(colour == Colour::WHITE) return Colour::BLACK;
+    if(colour == Colour::BLACK) return Colour::WHITE;
+    return Colour::NONE;
+}
+
+int pieceValue(PieceType pieceType){
+    switch(pieceType){
+        case PieceType::QUEEN:  return 9;
+        case PieceType::ROOK:   return 5;
+        case PieceType::BISHOP: return 3;
+        case PieceType::KNIGHT: return 3;
+        case PieceType::PAWN:   return 1;
+        case PieceType::KING:   return 100;
+        case PieceType::NONE:   return 0;
+    }
+    return 0;
+}
+
+PieceType pieceTypeFromChar(char ch){
+    switch(tolower(static_cast<unsigned char>(ch))){
+        case 'k': return PieceType::KING;
+        case 'q': return PieceType::QUEEN;
+        case 'b': return PieceType::BISHOP;
+        case 'r': return PieceType::ROOK;
+        case 'n': return PieceType::KNIGHT;
+        case 'p': return PieceType::PAWN;
+        default:  return PieceType::NONE;
+    }
+}
+
+Colour colourFromChar(char ch){
+    if(pieceTypeFromChar(ch) == PieceType::NONE) return Colour::NONE;
+    if(isupper(static_cast<unsigned char>(ch))) return Colour::WHITE;
+    return Colour::BLACK;
+}
 
 std::ostream& operator<<(std::ostream &out, PieceType pt){
     switch(pt){
