f7a5aa980df1bb3302dbf8f51cc32e7420257e57
Divy Shah <divyrshah3@gmail.com>
2026-09-15 19:43:38 -0400

    player: add a virtual destructor for unique_ptr ownership

--- player.h
+++ player.h
@@ -9,6 +9,7 @@ class Player {
 
     public:
     Player(Colour colour);
+    virtual ~Player() = default; // needed: players are owned through unique_ptr<Player>
     virtual Move getMove(Board *board) const = 0;
     Colour getColour() const;
 };
