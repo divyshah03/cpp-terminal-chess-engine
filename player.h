54ca66b2d8823b0d5c1771b1ce847c2af0ddbf95
Divy Shah <divyrshah3@gmail.com>
2026-09-15 19:43:38 -0400

    player: add usesTerminalInput to describe how a move is obtained

--- player.h
+++ player.h
@@ -11,6 +11,9 @@ class Player {
     Player(Colour colour);
     virtual ~Player() = default; // needed: players are owned through unique_ptr<Player>
     virtual Move getMove(Board *board) const = 0;
+    // True when getMove reads the squares from the terminal. A computer player does
+    // not, so whatever the user typed after the command has to be thrown away.
+    virtual bool usesTerminalInput() const;
     Colour getColour() const;
 };
 
