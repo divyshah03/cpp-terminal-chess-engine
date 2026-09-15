89e0ce32eb139240933ae30edb252fac8517a566
Divy Shah <divyrshah3@gmail.com>
2026-09-15 19:43:38 -0400

    state: give State default member values so copies are well defined

--- state.h
+++ state.h
@@ -9,11 +9,11 @@ enum class Direction {NW, N, NE, W, E, SW, S, SE, KNIGHT};
 
 struct State {
 
-    StateType type;  
-    Colour colour;   // What colour was the original notifying piece? (NOT what is my colour)
-    PieceType pieceType; // What type of piece is the original notifying piece?
-    Position position; // What is the position of the original notifying piece?
-    Direction direction; // Direction from the receiver to notifier
+    StateType type = StateType::EmptyCell;
+    Colour colour = Colour::NONE;         // What colour was the original notifying piece?
+    PieceType pieceType = PieceType::NONE; // What type of piece is the original notifying piece?
+    Position position{};                   // What is the position of the original notifying piece?
+    Direction direction = Direction::N;    // Direction from the receiver to notifier
 
 };
 
