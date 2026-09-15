e1e205f007ce379e0b7686e9c41b84c99c21562c
Divy Shah <divyrshah3@gmail.com>
2026-09-15 19:43:38 -0400

    enumerated: add MoveType and helpers for colours, values and characters

--- enumerated.h
+++ enumerated.h
@@ -27,6 +27,25 @@ enum class PieceType{
     NONE
 };
 
+// How a move has to be executed on the board
+enum class MoveType{
+    NORMAL,
+    CASTLE_KINGSIDE,
+    CASTLE_QUEENSIDE,
+    ENPASSANT,
+    PROMOTION
+};
+
+// The colour that is not the given colour; NONE for anything that is not a side
+Colour opponentOf(Colour colour);
+
+// Rough material worth of a piece, used by the computer players
+int pieceValue(PieceType pieceType);
+
+// Board configuration characters: uppercase is white, lowercase is black
+PieceType pieceTypeFromChar(char ch);
+Colour colourFromChar(char ch);
+
 // Output operator for PieceType
 std::ostream& operator<<(std::ostream &out, PieceType pt);
 
