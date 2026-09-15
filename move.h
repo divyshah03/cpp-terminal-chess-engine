d965c46b1dbf07e40f030eacbbb67760d65fc09f
Divy Shah <divyrshah3@gmail.com>
2026-09-15 19:43:38 -0400

    move: add isValid and sameSquares, and make the accessors const

--- move.h
+++ move.h
@@ -16,8 +16,12 @@ class Move {
          MoveType type = MoveType::NORMAL, PieceType promotion = PieceType::NONE);
     Position getFrom() const;
     Position getTo() const;
-    PieceType getPieceType();
-    bool isCaptured();
+    PieceType getPieceType() const;   // the piece this move captures
+    MoveType getMoveType() const;
+    PieceType getPromotion() const;
+    bool isCaptured() const;
+    bool isValid() const;             // both squares are on the board
+    bool sameSquares(const Move& other) const;
     bool operator==(const Move& other) const;
 };
 
