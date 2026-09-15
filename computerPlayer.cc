c7adf396c4c0644d357866082578b3ddfd4b0de0
Divy Shah <divyrshah3@gmail.com>
2026-09-15 19:43:39 -0400

    computerPlayer: play the best scoring move

--- computerPlayer.cc
+++ computerPlayer.cc
@@ -72,133 +72,14 @@ Move ComputerPlayer::getMove(Board *board) const {
         return validMoves[rand() % validMoves.size()];
     }
 
-        case 3:
-
-            // Prefers avoiding getting captured
-            for(Move mv : validMoves) {
-                for(Move attacked : opponentMoves) {
-                    if(!(mv == attacked)) {
-                        ret = mv;
-                        break;
-                    }
-                }
-            }
-
-        case 2:
-
-            //Prefers capturing moves
-            for(Move mv : validMoves) {
-                if(!(mv.getPieceType() == PieceType::NONE)) {
-                    ret = mv;
-                    break;
-                }
-            }
-
-            // Prefers checks
-            if(getColour() == Colour::WHITE)
-                posKing = board->getBKing();
-            else
-                posKing = board->getWKing();
-
-            for(Move mv : validMoves) {
-                //checker piece type is the piece type of the attacker
-                checker = board->getGrid()[mv.getFrom().getRowVector()][mv.getFrom().getColVector()].getPieceType();
-
-                // If attacker is knight
-                if(checker == PieceType::KNIGHT &&
-                   ((abs(mv.getTo().getRowVector() - posKing.getRowVector()) == 2 && 
-                    abs(mv.getTo().getColVector() - posKing.getColVector()) == 1) ||
-                   (abs(mv.getTo().getRowVector() - posKing.getRowVector()) == 1 && 
-                    abs(mv.getTo().getColVector() - posKing.getColVector()) == 2))) {
-                    ret = mv;
-                    break;
-                }
-
-                //If attacker is bishop
-                if(checker == PieceType::BISHOP &&
-                   abs(mv.getTo().getRowVector() - posKing.getRowVector()) == 
-                   abs(mv.getTo().getColVector() - posKing.getColVector())) {
-                    positionsBetween = Position::getAllPositions(mv.getTo(), posKing);
-
-                    //Checking if there is a blocker
-                    for(Position pos : positionsBetween) {
-                        if(board->getGrid()[pos.getRowVector()][pos.getColVector()].getPieceType() != PieceType::NONE){
-                            blocker = true;
-                            break;
-                        }
-                    }
-                    if(!blocker) {
-                        ret = mv;
-                        break;
-                    }
-                    
-                    blocker = false;
-                }
-
-                // If attacker is a rook
-                if(checker == PieceType::ROOK &&
-                   ((mv.getTo().getRowVector() - posKing.getRowVector()) == 0 ||
-                   (mv.getTo().getColVector() - posKing.getColVector()) == 0)) {
-                    positionsBetween = Position::getAllPositions(mv.getTo(), posKing);
-
-                    //Checking if there is a blocker
-                    for(Position pos : positionsBetween) {
-                        if(board->getGrid()[pos.getRowVector()][pos.getColVector()].getPieceType() != PieceType::NONE){
-                            blocker = true;
-                            break;
-                        }
-                    }
-                    if(!blocker) {
-                        ret = mv;
-                        break;
-                    }
-                    
-                    blocker = false;
-                }
-
-                if(checker == PieceType::QUEEN &&
-                   (((mv.getTo().getRowVector() - posKing.getRowVector()) == 0 ||
-                    (mv.getTo().getColVector() - posKing.getColVector()) == 0) ||
-                   (abs(mv.getTo().getRowVector() - posKing.getRowVector()) == 
-                    abs(mv.getTo().getColVector() - posKing.getColVector())))) {
-                        positionsBetween = Position::getAllPositions(mv.getTo(), posKing);
-
-                    //Checking if there is a blocker
-                    for(Position pos : positionsBetween) {
-                        if(board->getGrid()[pos.getRowVector()][pos.getColVector()].getPieceType() != PieceType::NONE){
-                            blocker = true;
-                            break;
-                        }
-                    }
-                    if(!blocker) {
-                        ret = mv;
-                        break;
-                    }
-                    
-                    blocker = false;
-                }
-
-                // Only other possible piece that can check is pawn
-                if(getColour() == Colour::WHITE) {
-                    if((posKing.getRow() - mv.getTo().getRow()) == 1 &&
-                        abs(posKing.getColInt() - mv.getTo().getRow()) == 1) {
-                            ret = mv;
-                            break;
-                    }
-                }
-
-                else if(getColour() == Colour::BLACK) {
-                    if((mv.getTo().getRow() - posKing.getRow()) == 1 &&
-                        abs(posKing.getColInt() - mv.getTo().getRow()) == 1) {
-                            ret = mv;
-                            break;
-                    }
-                }
-            }
-
-            // Play any move
-            ret = validMoves[0];
-            break;
+    Move best = validMoves[0];
+    int bestScore = scoreMove(board, best);
+    for (size_t i = 1; i < validMoves.size(); ++i) {
+        int score = scoreMove(board, validMoves[i]);
+        if (score > bestScore) {
+            bestScore = score;
+            best = validMoves[i];
+        }
     }
-    return ret;
+    return best;
 }
